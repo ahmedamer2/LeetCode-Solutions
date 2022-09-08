9542b53604f43bab0a162f767fda8aee0253b66c
Ahmed's Mackbook <ahmedsay01@gmail.com>
2022-09-07 22:03:59 -0600

    Finish merge two sorted linked lists

---
+++ Easy/MergeTwoSortedLists.java
@@ -0,0 +1,24 @@
+package Easy;
+
+import Classes.ListNode;
+
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode merged = new ListNode();
+        ListNode ptr = merged;
+
+        while(list1 != null || list2 != null){
+            if(list2 == null || (list1 != null && list1.val <= list2.val)){
+                ptr.next = list1;
+                list1 = list1.next;
+                ptr = ptr.next;
+            } else{
+                ptr.next = list2;
+                list2 = list2.next;
+                ptr = ptr.next;
+            }
+        }
+        
+        return merged.next;
+    }
+}
