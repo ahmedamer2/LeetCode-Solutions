3d73d6e8d4c647fcf652fe467ccaa38af0184ff7
Ahmed's Mackbook <ahmedsay01@gmail.com>
2022-09-22 18:22:46 -0600

    Finished invert binary tree; and Container with most water

---
+++ Medium/ContainerWithMostWater.java
@@ -0,0 +1,21 @@
+package Medium;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1, width = height.length-1;
+        int highest = 0;
+        while(left < right){
+            int lowerSide = height[left] < height[right] ? height[left] : height[right];
+            if(lowerSide * width > highest){
+                highest = lowerSide*width;
+            } 
+            if(height[left] > height[right]){
+                right--;
+            }else{
+                left++;
+            }
+            width--;
+        }
+        return highest;
+    }
+}
