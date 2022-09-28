93796935caecdccc0bf6691b160ca4cb1d364f1f
Ahmed's Mackbook <ahmedsay01@gmail.com>
2022-09-28 10:20:12 -0600

    Finished SameTree

---
+++ Easy/SameTree.java
@@ -0,0 +1,33 @@
+package Easy;
+
+import Classes.TreeNode;
+
+public class SameTree {
+    boolean sameTree = true;
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        traverse(p, q);
+        return sameTree;
+    }
+
+    public void traverse(TreeNode p, TreeNode q) {
+        
+        // tree has been determined to be not the same so return
+        if(!sameTree) {
+            return;
+        }
+
+        if(p == null && q == null){
+            return;
+        }
+
+        // found a node on one side but not the other
+        if((p == null && q != null) || (p != null && q == null) || p.val != q.val){
+            sameTree = false;
+            return;
+        }
+
+        traverse(p.right, q.right);
+        traverse(p.left, q.left);
+    }
+}
