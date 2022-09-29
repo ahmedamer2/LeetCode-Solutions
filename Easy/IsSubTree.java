57112d0baab27eae7351e1c258ac5c35ed8e40f9
Ahmed's Mackbook <ahmedsay01@gmail.com>
2022-09-29 17:32:58 -0600

    finished isSubTree

---
+++ Easy/IsSubTree.java
@@ -0,0 +1,36 @@
+package Easy;
+
+import Classes.TreeNode;
+
+class Solution {
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null && root == null) {
+            return true;
+        }
+        if (root == null || subRoot == null) {
+            return false;
+        }
+
+        if (isSameTree(root, subRoot)) {
+            return true;
+        }
+
+        return isSubtree(root.right, subRoot) || isSubtree(root.left, subRoot);
+    }
+
+    public boolean isSameTree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null && root == null) {
+            return true;
+        }
+        if (root == null || subRoot == null) {
+            return false;
+        }
+
+        if (root.val == subRoot.val) {
+            return isSameTree(root.right, subRoot.right) && isSameTree(root.left, subRoot.left);
+        }
+
+        return false;
+    }
+}
