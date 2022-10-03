83383fba96b6504fd2c22ec8f4ace0b69b56a320
ahmedamer2 <63519817+ahmedamer2@users.noreply.github.com>
2022-10-02 23:31:55 -0600

    finished binary search

--- Easy/BinarySearch.java
+++ Easy/BinarySearch.java
@@ -2,27 +2,24 @@ package Easy;
 
 public class BinarySearch {
     public int search(int[] nums, int target) {
-        if (nums.length == 0) {
+        if(nums.length == 1 && nums[0] != target){
             return -1;
         }
 
-        int middle = (nums.length / 2) - 1;
-
-        return BinarySearch(nums, 0, nums.length, middle, target);
-    }
-
-    public int BinarySearch(int[] nums, int start, int end, int middle, int target) {
-        if (nums[middle] == target) {
-            return middle;
+        int start = 0, end = nums.length-1;
+        while(start <= end){
+            int middle = (start+ end)/2;
+            if(nums[middle] == target){
+                return middle;
+            }
+            else if(nums[middle] > target){
+                end = middle - 1;
+            }
+            else{
+                start = middle + 1;
+            }
         }
-
-        if (nums[middle] > target) {
-            return BinarySearch(nums, start, middle / 2, middle / 4, target);
-        }
-        if (nums[middle] < target) {
-            return BinarySearch(nums, middle, end, (end - middle) / 2, target);
-        }
-
         return -1;
     }
+
 }
