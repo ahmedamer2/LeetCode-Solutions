2c00b27f566059165ca9dfb19668d6dcf7534cf8
ahmedamer2 <63519817+ahmedamer2@users.noreply.github.com>
2022-10-08 23:31:44 -0600

    Saving changes

--- Medium/test2.java
+++ Medium/test2.java
@@ -4,15 +4,27 @@ import java.util.List;
 
 public class test2 {
     public List<boolean> findPrimes(int n){
-        return null;
+        List<Boolean> primes = new ArrayList<Boolean>(n+1);
+        for(int i = 0; i < primes.size(); i++){
+            primes.set(i, true);
+        }
+
+        for(int j = 2; j*j <= Math.sqrt(n); j++){
+            if(primes.get(j) == true){
+                for(int k = j *j; k <= Math.sqrt(n); k += j){
+                    primes.set(k, false);
+                }
+            }
+        }
+        System.out.print();
+        return primes;
     }
 
 
     public static List<Integer> lightBulbs(List<Integer> states, List<Integer> numbers) {
         ArrayList<Boolean> primes = findPrimes(states.size());
 
-
-        return primes;
+        return ;
     }
     
     public static List<String> splitWords(String s) {
