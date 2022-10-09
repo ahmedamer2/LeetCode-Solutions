def0dafe50f233d30d2f19f54795b6625faf58cb
ahmedamer2 <63519817+ahmedamer2@users.noreply.github.com>
2022-10-08 23:52:21 -0600

    Finished lowest common ancestor

--- Trees/LowestCommonAncestor.java
+++ Trees/LowestCommonAncestor.java
@@ -4,6 +4,14 @@ import Classes.TreeNode;
 
 public class LowestCommonAncestor {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
+        //BST
+
+        if(p.val > root.val && q.val > root.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        if(p.val < root.val && q.val < root.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
     }
 }
